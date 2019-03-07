d89a4fd0b592d6eea6b980261a9fd811064de694
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2019-03-07 11:33:45 -0600

    reformatted most of the code

--- AVRUtils.h
+++ AVRUtils.h
@@ -17,47 +17,52 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef _AVRUTILS_
-#define _AVRUTILS_
+#ifndef _AVRUTILS_H__
+#define _AVRUTILS_H__
+
 
 #include <Arduino.h>
+
+
+
 class AVRUtils {
-	public:
+public:
+
+	/**
+	 * Creates an instance to monitor battery voltage and level.
+	 *
+	 * @param minVoltage is the voltage, expressed in millivolts, corresponding to an empty battery
+	 * @param maxVoltage is the voltage, expressed in millivolts, corresponding to a full battery
+	 * @param sensePin is the analog pin used for sensing the battery voltage
+	 * @param activationPin is the optional digital pin which will be turned LOW before starting the battery sensing,
+	 *            useful when using a resistor divider to save on battery consumption
+	 */
+	AVRUtils(void);
+
+	/**
+	 * Initializes the library by optionally setting the board voltage level, expressed in millivolts, (defaults to 5000)
+	 * and the resistor divider ratio (defaults to 2, an equal divider).
+	 * To obtain the best results use a calibrated reference using the VoltageReference library or equivalent.
+	 */
+	void begin(double AVRTempCorr = 324.31, double AVRVccCorr = 1125300L);
+
+	/**
+	 * Returns the current battery level as a number between 0 and 100, with 0 indicating an empty battery and 100 a
+	 * completely full battery.
+	 */
+	double getAVRTemp(void);
+
+	/**
+	 * Returns the current battery voltage in millivolts.
+	 */
+	double getAVRVcc(void);
 
 
-		/**
-		 * Creates an instance to monitor battery voltage and level.
-		 *
-		 * @param minVoltage is the voltage, expressed in millivolts, corresponding to an empty battery
-		 * @param maxVoltage is the voltage, expressed in millivolts, corresponding to a full battery
-		 * @param sensePin is the analog pin used for sensing the battery voltage
-		 * @param activationPin is the optional digital pin which will be turned LOW before starting the battery sensing,
-		 *            useful when using a resistor divider to save on battery consumption
-		 */
-		AVRUtils(void);
+private:
 
-		/**
-		 * Initializes the library by optionally setting the board voltage level, expressed in millivolts, (defaults to 5000)
-		 * and the resistor divider ratio (defaults to 2, an equal divider).
-		 * To obtain the best results use a calibrated reference using the VoltageReference library or equivalent.
-		 */
-		void begin(double AVRTempCorr = 324.31, double AVRVccCorr = 1125300L);
-
-		/**
-		 * Returns the current battery level as a number between 0 and 100, with 0 indicating an empty battery and 100 a
-		 * completely full battery.
-		 */
-		double getAVRTemp(void);
-
-		/**
-		 * Returns the current battery voltage in millivolts.
-		 */
-		double getAVRVcc(void);
-
-
-	private:
-		double AVRTempCorr;
-        double AVRVccCorr;
+	double AVRTempCorr;
+	double AVRVccCorr;
 };
 
-#endif // _AVRUtils_
+
+#endif	/* !_AVRUTILS_H__ */
